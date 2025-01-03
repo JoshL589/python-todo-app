0399b0503804a7c7f52d2ffdcc6bdfd5edf3949c
Joshua Li <joshuazli2020@email.com>
2025-01-03 11:12:53 +1000

    following a tutorial

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
-name = input("enter your name: ")
+tasks = []
 
-print("hello " + name)
+if __name__ == "__main__":
+    ### Create a loop to run the app
+    print("Welcome to the to do list app :)")
+    while True:
+        print()
